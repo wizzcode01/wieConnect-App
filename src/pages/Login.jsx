6f9dd6eb1644c485f537a31870c160cc72310505
wizzycode <dicksonwisdom005@gmail.com>
2025-09-29 12:30:07 +0100

    updates

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -3,22 +3,29 @@ import { Link } from "react-router-dom";
 
 const Login = () => {
     return (
-        <div className="min-h-screen overflow-hidden w-full relative flex flex-col items-center justify-center  bg-gray-900">
-             <div className="w-full lg:ml-0 flex-col gap-5 justify-center items-center text-center">
+        <div className="min-h-screen overflow-hidden w-full relative flex flex-col items-center justify-center gap-5 bg-gray-900">
+            <div className="">
+               <div className="w-full lg:ml-0 flex-col gap-5 justify-center items-center text-center">
                           {/* <h1 className="text-white font-bold  border border-white rounded-lg p-1 px-4 cursor-pointer" 
                            onClick={() => setStep(1)}
                           ><FaArrowLeft size={20}/></h1>  */}
-                          <h1 className="text-5xl m-5 font-bold bg-gradient-to-r from-purple-500 to-pink-600 bg-clip-text text-transparent">wieConnect</h1>
+                          <h1 className="text-5xl m-5 font-bold bg-gradient-to-r from-purple-700 to-pink-600 bg-clip-text text-transparent">wieConnect</h1>
                           <p className="text-[16px] m-5 text-gray-400 font-bold ">Login With any of The Following</p>
-                      </div>    
-                      <div className="flex justify-center items-center gap-5">
+                </div>    
+                <div className="flex justify-center items-center gap-5">
                       <FaGoogle className=" bg-gray-900 cursor-pointer text-gray-100 border-2 rounded-lg border-[#9003d6] text-4xl w-28 p-2"/>
                       <FaApple className="text-gray-100 cursor-pointer border-2 border-[#9003d6] rounded-lg bg-gray-900 text-4xl w-28 p-2"/>
-                    </div>   
-        <div className="flex flex-col items-center w-full max-w-md bg-gray-900 px-8 py-10 ">
-           <span className="text-center text-xl text-gray-300">OR</span>
+               </div> 
+            </div>  
+
+            <div className="flex justify-center items-center w-full my-4">
+                <hr className="w-20 border-gray-700" />
+                <span className="mx-4 text-sm text-gray-300 font-semibold">OR</span>
+                <hr className="w-20 border-gray-700" />
+            </div>       
+        <div className="flex flex-col items-center w-full max-w-md bg-gray-900 px-6">      
             <form 
-            className="flex flex-col w-full mt-5 gap-2">
+            className="flex flex-col w-full gap-2">
                 <div className="flex flex-col gap-6">
                    <div className="">
                         <input 
--- src/pages/Registration.jsx
+++ src/pages/Registration.jsx
@@ -17,23 +17,28 @@ const Registration = () => {
 
     if(step === 1) {
       return (
-        <div className="min-h-screen w-full relative flex flex-col items-center justify-center  bg-gray-900">
+        <div className="min-h-screen w-full relative flex flex-col items-center justify-center gap-5 bg-gray-900">
+          <div>
             <div className="w-full lg:ml-0 flex-col gap-5 justify-center items-center text-center">
                 {/* <h1 className="text-white font-bold  border border-white rounded-lg p-1 px-4 cursor-pointer" 
                  onClick={() => setStep(1)}
                 ><FaArrowLeft size={20}/></h1>  */}
-                <h1 className="text-5xl m-5 font-bold bg-gradient-to-r from-purple-500 to-pink-600 bg-clip-text text-transparent">wieConnect</h1>
+                <h1 className="text-5xl m-5 font-bold bg-gradient-to-r from-purple-700 to-pink-600 bg-clip-text text-transparent">wieConnect</h1>
                 <p className="text-[16px] m-5 text-gray-400 font-bold ">SignUp With any of The Following</p>
             </div>    
             <div className="flex justify-center items-center gap-5">
             <FaGoogle className=" bg-gray-900 cursor-pointer text-gray-100 border-2 rounded-lg border-[#9003d6] text-4xl w-28 p-2"/>
             <FaApple className="text-gray-100 cursor-pointer border-2 border-[#9003d6] rounded-lg bg-gray-900 text-4xl w-28 p-2"/>
+           </div> 
+          </div> 
+          <div className="flex justify-center items-center w-full my-4">
+            <hr className="w-20 border-gray-700" />
+            <span className="mx-4 text-sm text-gray-300 font-semibold">OR</span>
+            <hr className="w-20 border-gray-700" />
           </div>  
-
-        <div className="flex flex-col items-center w-full max-w-md bg-gray-900 px-8 py-10 ">
-             <span className="text-center text-xl text-gray-300">OR</span>
+        <div className="flex flex-col items-center w-full max-w-md bg-gray-900 px-6 "> 
             <form 
-            className="flex flex-col w-full mt-5 gap-2">
+            className="flex flex-col w-full gap-2">
                 <div className="flex flex-col gap-6">
                    <div className="">
                         <input 
@@ -57,10 +62,9 @@ const Registration = () => {
                   className="bg-[#9003d6] font-bold text-gray-100 text-[1.5rem] px-4 py-2 mt-5 m-auto rounded-4xl w-72 hover:bg-white hover:text-[#4a006f] transition-all ease-in-out duration-300 cursor-pointer"
                   type="submit"
                 >
-                   <div className="flex items-center text-center gap-2">
-                    
+  
                      <FaArrowRight size={30} className="text-center m-auto"/>
-                   </div>
+                 
                 </button>
             </form>
         </div>
@@ -79,11 +83,11 @@ const Registration = () => {
     return (
          <div className="min-h-screen w-full relative flex flex-col items-center justify-center  bg-gray-900">
             <div className="w-full flex gap-5 justify-center items-center ">
-                <h1 className="text-4xl font-bold bg-gradient-to-r from-purple-500 to-pink-600 bg-clip-text text-transparent">WieConnect</h1>
+                <h1 className="text-4xl font-bold bg-gradient-to-r from-purple-700 to-pink-600 bg-clip-text text-transparent">WieConnect</h1>
             </div>    
            <div className="flex flex-col items-center w-full max-w-md bg-gray-900 px-8 py-10 ">
             <form 
-            className="flex flex-col w-full  gap-2">
+            className="flex flex-col w-full gap-2">
                 <div className="flex flex-col gap-6">
                    <div className="">
                         <input 
@@ -116,7 +120,7 @@ const Registration = () => {
         <div className="flex flex-col justify-center items-center gap-3 mt-3 p-3">
            <p
             className="text-gray-500 text-sm">
-           Already have an account?{" "} <Link to="/" className="text-gray-100">Log In</Link></p> 
+           Already have an account?{" "} <Link to="/login" className="text-gray-100">Log In</Link></p> 
         </div>
     </div>
     )
